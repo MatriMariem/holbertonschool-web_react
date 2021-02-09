0e1e87d62929195a5e5f945bf9ca931db5e52b47
MatriMariem <meriemmatri1994@gmail.com>
2021-02-09 23:19:36 +0100

    class and constructor interface

---
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -0,0 +1,41 @@
+interface DirectorInterface {
+  workFromHome(): string,
+  getCoffeeBreak(): string,
+  workDirectorTasks(): string,
+}
+interface TeacherInterface {
+  workFromHome(): string,
+  getCoffeeBreak(): string,
+  workTeacherTasks(): string,
+}
+
+export class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+  getToWork(): string {
+    return "Getting a coffee break";
+  }
+  workDirectorTasks(): string {
+    return "Getting to director tasks"
+  }
+}
+export class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+  workTeacherTasks(): string {
+    return "Getting to work"
+  }
+}
+
+export const createEmployee = function(salary: number | string): Director | Teacher {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  } else {
+    return Director();
+  }
+}
