bdabe69204860809850b1c4d9b60c49bfa54831d
MatriMariem <meriemmatri1994@gmail.com>
2021-02-11 16:42:08 +0100

    task 7

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -51,3 +51,13 @@ export function executeWork(employee: DirectorInterface | TeacherInterface): str
     return(employee.workTeacherTasks());
   }
 }
+
+type Subjects = 'Math' | 'History';
+
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return "Teaching Math";
+  } else if (todayClass === 'History') {
+    return "Teaching History";
+  }
+}
