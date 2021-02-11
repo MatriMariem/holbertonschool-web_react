fd3ab761270725775dd22cdf5b0e2fce83c779f1
MatriMariem <meriemmatri1994@gmail.com>
2021-02-11 17:22:15 +0100

    task 8

--- 0x00-TypeScript/task_3/js/main.ts
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -8,16 +8,16 @@ const row: RowElement = {
   lastName: 'Salva',
 };
 const newRowID: RowID = CRUD.insertRow(row);
-const updatedRow: RowElement = { firstName: "Guillaume", lastName: "Salva", age: 23 };
+const updatedRow: RowElement = { firstName: 'Guillaume', lastName: 'Salva', age: 23 };
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
 
 //
-const obj = {firstName: "Guillaume", lastName: "Salva"};
+const obj = {firstName: 'Guillaume', lastName: 'Salva'};
 CRUD.insertRow(obj)
 // Insert row {firstName: "Guillaume", lastName: "Salva"}
 
-const updatedRow: RowElement = { firstName: "Guillaume", lastName: "Salva", age: 23 };
+const updatedRow: RowElement = { firstName: 'Guillaume', lastName: 'Salva', age: 23 };
 CRUD.updateRow(newRowID, updatedRow);
 // Update row 125 {firstName: "Guillaume", lastName: "Salva", age: 23}
 
