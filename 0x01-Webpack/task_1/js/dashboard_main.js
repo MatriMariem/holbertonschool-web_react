704585d45f9a4e4eb29c87c2c53258a764a978e6
MatriMariem <meriemmatri1994@gmail.com>
2021-02-12 22:25:51 +0100

    webpack with config file

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,23 +1,18 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
 let count = 0;
 
 function updateCounter() {
 	count += 1;
+  $("#count").text(`${count} clicks on the button`);
 	return count;
 }
-
-$(function() {
-	$('body').append('<p>Holberton Dashboard</p>');
-	$('body').append('<p>Dashboard data for the students</p>');
-	$('body').append('<button>Click here to get started</button>');
-	$('body').append("<p id='count'></p>");
-	$('body').append('<p>Copyright - Holberton School</p>');
-
-	let debouncedFunc = _.debounce(() => {
+$('button').on('click', _.debounce(() => {
 		let count = updateCounter();
-		$('#count').text(`${count} clicks on the button`);
-	});
-	$('button').on('click', debouncedFunc);
-});
+	}));
