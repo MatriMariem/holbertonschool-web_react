67635eda83142e3638fea1dad9326652c2dccf90
MatriMariem <meriemmatri1994@gmail.com>
2021-03-15 16:10:29 +0100

    Add notifications, Use data attributes and inline styling, Task 3: Done

--- 0x02-react_intro/task_2/dashboard/src/utils.js
+++ 0x02-react_intro/task_2/dashboard/src/utils.js
@@ -7,5 +7,7 @@ function getFullYear () {
 function getFooterCopy (isIndex) {
   return (isIndex ? 'Holberton School' : 'Holberton School main dashboard');
 }
-
-export { getFullYear, getFooterCopy }
+function getLatestNotification () {
+  return ('<strong>Urgent requirement</strong> - complete by EOD');
+}
+export { getFullYear, getFooterCopy, getLatestNotification }
