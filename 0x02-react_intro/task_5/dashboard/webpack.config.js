c6500a1a92048f0c67237817ebe67e66b31f6dc3
MatriMariem <meriemmatri1994@gmail.com>
2021-03-26 21:27:57 +0100

    task 8 uncompleted

---
+++ 0x02-react_intro/task_5/dashboard/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        loader: 'image-webpack-loader',
+        enforce: 'pre'
+      }
+    ]
+  }
+};
